260976e5bf21031f2b619fdc4eb91d6a5a0457d4
Marta Lapina <marta.lapina@accenture.com>
2016-05-01 19:27:48 +0300

    display a list of countries and add filter/search

--- script.js
+++ script.js
@@ -30,12 +30,10 @@
     // create the controller and inject Angular's $scope
     myApp.controller('mainController', function($scope, $http) {
     
-        $http.get('countries.json').success(function(data) {
+       $http.get('countries.json').success(function(data) {
 	       $scope.countries = data;
 	   });
-
-        // create a message to display in our view
-        $scope.message = 'Everyone come and see how good I look!';
+       $scope.countryFilter   = '';     // set the default search/filter term
     });
 	
 	myApp.controller('aboutController', function($scope) {
